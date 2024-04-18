b7505c35696cff2d6e9045dd06cbaa522583d75b
Minura Dinupriya <mranaweera793@gmail.com>
2024-04-18 21:22:31 +0530

    Create Route package

--- api/app.js
+++ api/app.js
@@ -5,6 +5,21 @@ const app=express();
 app.use("/api/test",(req, res)=>{
     res.send("It works")
 })
+app.use("/api/auth/register",(req, res)=>{
+    res.send("It works")
+})
+app.use("/api/auth/login",(req, res)=>{
+    res.send("It works")
+})
+app.use("/api/auth/logout",(req, res)=>{
+    res.send("It works")
+})
+app.use("/api/posts",(req, res)=>{
+    res.send("It works")
+})
+app.use("/api/post/12312",(req, res)=>{
+    res.send("It works")
+})
 
 app.listen(8800,()=>{
     console.log("Server is running");
