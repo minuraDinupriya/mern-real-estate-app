0839999e3441d7951bba2fade119e98187e0f63e
Minura Dinupriya <mranaweera793@gmail.com>
2024-04-29 10:15:55 +0530

    Add bcrypt and hash the password

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,13 @@
-export const register=(req,res)=>{
-    //dp operations
-    console.log(req.body);
+import bcrypt from "bcrypt";
+
+export const register=async (req,res)=>{
+    const {username, email, password}=req.body;
+
+    // hashing the password
+    const hashedPassword=await bcrypt.hash(password,10)
+    console.log(hashedPassword);
+
+    // create a new user and save to db
 }
 export const login=(req,res)=>{
     //dp operations
