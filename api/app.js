db397c59fd43ba2becc2e8a608b7bbd032fad9e9
Minura Dinupriya <mranaweera793@gmail.com>
2024-06-07 16:27:59 +0530

    Add response messege when creating user

--- api/app.js
+++ api/app.js
@@ -3,6 +3,7 @@ import postRoute from "./routes/post.route.js"
 import authRoute from "./routes/auth.route.js"
 
 const app=express();
+console.log(1);
 
 app.use(express.json())
 
--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -4,6 +4,7 @@ import prisma from "../lib/prisma.js";
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
 
+  try{
   // hashing the password
   const hashedPassword = await bcrypt.hash(password, 10);
 //   console.log(hashedPassword);
@@ -17,6 +18,12 @@ export const register = async (req, res) => {
     },
   });
   console.log(newUser);
+   res.status(201).json({messsege:"User created successfully"})
+   }catch(err){
+     console.log(err);
+     res.status(500).json({messsege:"Failed to create user"})
+
+  }
 };
 export const login = (req, res) => {
   //dp operations
