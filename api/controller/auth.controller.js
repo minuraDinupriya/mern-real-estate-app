d09399cb9440ce6dccf2f9db25663ef9694d8301
Minura Dinupriya <mranaweera793@gmail.com>
2024-06-07 10:18:20 +0530

    Update Prisma schema

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,18 +1,26 @@
 import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
 
-export const register=async (req,res)=>{
-    const {username, email, password}=req.body;
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
 
-    // hashing the password
-    const hashedPassword=await bcrypt.hash(password,10)
-    console.log(hashedPassword);
+  // hashing the password
+  const hashedPassword = await bcrypt.hash(password, 10);
+//   console.log(hashedPassword);
 
-    // create a new user and save to db
-    const newUser=await
-}
-export const login=(req,res)=>{
-    //dp operations
-}
-export const logout=(req,res)=>{
-    //dp operations
-}
+  // create a new user and save to db
+  const newUser = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password: hashedPassword,
+    },
+  });
+  console.log(newUser);
+};
+export const login = (req, res) => {
+  //dp operations
+};
+export const logout = (req, res) => {
+  //dp operations
+};
