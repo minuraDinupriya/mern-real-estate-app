c3423fd13e3802c631a12797c86a1fc1b37d525e
Minura Dinupriya <mranaweera793@gmail.com>
2024-07-26 21:08:59 +0530

    Minor update in app.js

--- api/app.js
+++ api/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
+import testRoute from "./routes/test.route.js";
 
 const app = express();
 
@@ -10,8 +11,9 @@ app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/test", testRoute);
 
 app.listen(8800, () => {
   console.log("Server is running");
