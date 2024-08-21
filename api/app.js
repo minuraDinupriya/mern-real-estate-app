c72db0b8c9492d46e6af91b456e8ff7340238081
Minura Dinupriya <mranaweera793@gmail.com>
2024-08-21 20:47:08 +0530

    Add user route

--- api/app.js
+++ api/app.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
 import testRoute from "./routes/test.route.js";
+import userRoute from "./routes/user.route.js";
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/test", testRoute);
 
