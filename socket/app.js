38418d8e3b01f6bbad8d2ff5fe8ae87fe6940b30
Minura Dinupriya <mranaweera793@gmail.com>
2024-09-30 15:01:17 +0530

    Create addduser request

---
+++ socket/app.js
@@ -0,0 +1,20 @@
+import { Server } from "socket.io";
+
+const io = new Server({
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+let onlineUser = [];
+
+const addUser = (userId, socketId) => {
+  const userExits = onlineUser.find((user) => user.userId === userId);
+  if (!userExits) {
+    onlineUser.push({ userId, socketId });
+  }
+};
+
+
+
+io.listen("4000");
