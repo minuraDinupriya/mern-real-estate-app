a1c99c2f5d58e70be55e63ce4dda61bc77dc8fa8
Minura Dinupriya <mranaweera793@gmail.com>
2024-10-03 13:54:35 +0530

    Add socket io handlers

--- socket/app.js
+++ socket/app.js
@@ -23,6 +23,19 @@ const getUser = (userId) => {
   return onlineUser.find((user) => user.userId === userId);
 };
 
+io.on("connection", (socket) => {
+  socket.on("newUser", (userId) => {
+    addUser(userId, socket.id);
+  });
 
+  socket.on("sendMessage", ({ receiverId, data }) => {
+    const receiver = getUser(receiverId);
+    io.to(receiver.socketId).emit("getMessage", data);
+  });
+
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+  });
+});
 
 io.listen("4000");
